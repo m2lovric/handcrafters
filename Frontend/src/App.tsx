2a16392cd0dd2d4b0af4616a9ea37e37bb8f7cc9
m2lovric <m2lovric@gmail.com>
2022-02-02 00:17:28 +0100

    Import stock photos

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import { Layout } from './components/Layout';
+import './App.scss';
 
 function App() {
   return (
     <Layout>
       <main className='container'>
-        <h1>Homepage</h1>
+        <section className='photo-grid-container'>
+          <section className='photo-grid'>
+            <img
+              src='./images/battlecreek-coffee-roasters-rsnzc-8dVs0-unsplash.jpg'
+              alt=''
+            />
+            <img src='./images/c-d-x-PDX_a_82obo-unsplash.jpg' alt='' />
+            <img
+              src='./images/charles-deluvio-1-nx1QR5dTE-unsplash.jpg'
+              alt=''
+            />
+            <img src='./images/daniel-korpai-hbTKIbuMmBI-unsplash.jpg' alt='' />
+            <img src='./images/eniko-kis-KsLPTsYaqIQ-unsplash.jpg' alt='' />
+            <img
+              src='./images/fernando-andrade-potCPE_Cw8A-unsplash.jpg'
+              alt=''
+            />
+            <img
+              src='./images/giorgio-trovato-K62u25Jk6vo-unsplash.jpg'
+              alt=''
+            />
+            <img src='./images/imani-bahati-LxVxPA1LOVM-unsplash.jpg' alt='' />
+            <img src='./images/jakob-owens-O_bhy3TnSYU-unsplash.jpg' alt='' />
+            <img src='./images/rachit-tank-2cFZ_FB08UM-unsplash.jpg' alt='' />
+            <img src='./images/revolt-164_6wVEHfI-unsplash.jpg' alt='' />
+            <img
+              src='./images/ruslan-bardash-4kTbAMRAHtQ-unsplash.jpg'
+              alt=''
+            />
+            <img src='./images/varun-gaba-dcgB3CgidlU-unsplash.jpg' alt='' />
+            <img src='./images/alex-motoc-0POwK6iAiRQ-unsplash.jpg' alt='' />
+          </section>
+        </section>
       </main>
     </Layout>
   );
